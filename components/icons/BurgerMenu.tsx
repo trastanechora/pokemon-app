18f8629811e7fc1c5be14ae11e44c1dc7b043c82
trastanechora <trastanechora@gmail.com>
2021-04-09 20:54:26 +0700

    Add burgermenu icon component

---
+++ components/icons/BurgerMenu.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+function BurgerMenu(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 100 80" width="20" height="20" {...props}>
+      <rect fill={props.color} width="100" height="12" rx="8"></rect>
+      <rect fill={props.color} y="30" width="100" height="12" rx="8"></rect>
+      <rect fill={props.color} y="60" width="100" height="12" rx="8"></rect>
+  </svg>
+  )
+}
+
+export default BurgerMenu
