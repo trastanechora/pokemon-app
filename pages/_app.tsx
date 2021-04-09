6234071ecf3e5200c459d61a6003bc04797eb02d
trastanechora <trastanechora@gmail.com>
2021-04-09 23:37:46 +0700

    Add apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from 'next/app'
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+  uri: 'https://graphql-pokeapi.vercel.app/api/graphql',
+  cache: new InMemoryCache()
+});
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <>   
-    <Component {...pageProps} />
+  <>
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
   </>
 )
 
