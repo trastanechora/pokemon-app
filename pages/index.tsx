da327eb058c4011ce0c30d162f1d772e3f71cd9e
trastanechora <trastanechora@gmail.com>
2021-04-09 22:23:45 +0700

    Add query get pokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react'
-
 import DefaultLayout from '../layout/Default'
 import Button from '../components/Button'
+
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+  uri: 'https://graphql-pokeapi.vercel.app/api/graphql',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query pokemons($limit: Int, $offset: Int) {
+        pokemons(limit: $limit, offset: $offset) {
+          count
+          next
+          previous
+          status
+          message
+          results {
+            url
+            name
+            image
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.warn(result));
 
 
 const Home = () => {
   return (
-    <DefaultLayout headerTitle="Pokemon App">
-       <Button>Test Button</Button>
-    </DefaultLayout>
+    <ApolloProvider client={client}>
+      <DefaultLayout headerTitle="Pokemon App">
+        <Button>Test Button</Button>
+      </DefaultLayout>
+    </ApolloProvider>
   )
 }
 
