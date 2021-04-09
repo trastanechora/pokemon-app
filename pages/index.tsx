387435f1f2677bbd5d9fd744d0110eb4cb473e6a
trastanechora <trastanechora@gmail.com>
2021-04-09 23:38:12 +0700

    Change fetch method with useQuery

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,49 @@
 import DefaultLayout from '../layout/Default'
 import Button from '../components/Button'
 
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
 
-const client = new ApolloClient({
-  uri: 'https://graphql-pokeapi.vercel.app/api/graphql',
-  cache: new InMemoryCache()
-});
-
-client
-  .query({
-    query: gql`
-      query pokemons($limit: Int, $offset: Int) {
-        pokemons(limit: $limit, offset: $offset) {
-          count
-          next
-          previous
-          status
-          message
-          results {
-            url
-            name
-            image
-          }
-        }
+const GET_POKEMONS = gql`
+  query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      count
+      next
+      previous
+      status
+      message
+      results {
+        url
+        name
+        image
       }
-    `
-  })
-  .then(result => console.warn(result));
+    }
+  }
+`;
 
+const gqlVariables = {
+  limit: 2,
+  offset: 1,
+};
+
+export const FetchPokemon = () => {
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: gqlVariables,
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error! {error.message}</div>;
+
+  console.warn('Response from server', data);
+  return <div>Success!</div>;
+};
 
 const Home = () => {
   return (
-    <ApolloProvider client={client}>
-      <DefaultLayout headerTitle="Pokemon App">
-        <Button>Test Button</Button>
-      </DefaultLayout>
-    </ApolloProvider>
+    <DefaultLayout headerTitle="Pokemon App">
+      <Button>Test Button</Button>
+      <FetchPokemon />
+    </DefaultLayout>
   )
 }
 
