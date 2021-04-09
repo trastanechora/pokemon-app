47736ada69c5ba5e8f438b28267f706933cf7332
trastanechora <trastanechora@gmail.com>
2021-04-09 21:48:13 +0700

    Add my-pokemon page

---
+++ pages/my-pokemon/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import DefaultLayout from '../../layout/Default'
+import Button from '../../components/Button'
+
+
+const MyPokemon = () => {
+  return (
+    <DefaultLayout>
+       MY POKEMON
+    </DefaultLayout>
+  )
+}
+
+export default MyPokemon
