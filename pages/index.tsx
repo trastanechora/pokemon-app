97eb6939e05960e7ceed761fae55605bc3c2c2cb
trastanechora <trastanechora@gmail.com>
2021-04-09 16:40:16 +0700

    Convert index page into tsx

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import DefaultLayout from '../layout/Default'
+import Button from '../components/Button'
+
+
+const Home = () => {
+  return (
+    <DefaultLayout headerTitle="Pokemon App">
+      Home
+       <Button>Test Button</Button>
+    </DefaultLayout>
+  )
+}
+
+export default Home
