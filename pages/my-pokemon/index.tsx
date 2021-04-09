4bfc17a547787d67c57963a25887ef83de480752
trastanechora <trastanechora@gmail.com>
2021-04-09 23:38:52 +0700

    Update my-pokemon page with qql request template

--- pages/my-pokemon/index.tsx
+++ pages/my-pokemon/index.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import { useQuery, gql } from '@apollo/client';
 
 import DefaultLayout from '../../layout/Default'
-import Button from '../../components/Button'
 
+const GET_POKEMONS = gql`
+  query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      count
+      next
+      previous
+      status
+      message
+      results {
+        url
+        name
+        image
+      }
+    }
+  }
+`;
+
+const gqlVariables = {
+  limit: 2,
+  offset: 1,
+};
+
+export const FetchDetail = () => {
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: gqlVariables,
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error! {error.message}</div>;
+
+  console.warn('Response from server', data);
+  return <div>Success!</div>;
+};
 
 const MyPokemon = () => {
   return (
     <DefaultLayout>
-       MY POKEMON
+       <FetchDetail />
     </DefaultLayout>
   )
 }
