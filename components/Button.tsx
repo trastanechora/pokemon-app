60418ad62c3d9a5b18b3fccea29328c2d459573a
trastanechora <trastanechora@gmail.com>
2021-04-09 20:47:01 +0700

    Modify button component's layout

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx, css } from '@emotion/react'
+import { jsx, css, keyframes } from '@emotion/react'
 import React from 'react'
 
 import { createRippleEffect } from '../utils'
@@ -26,25 +26,28 @@ const Button = ({ type, onClick, children }: IButtonProps) => {
     }
   };
 
+  const baseButton = css`
+    position: relative;
+    overflow: hidden;
+    transition: background 400ms;
+    color: #fff;
+    background-color: #6200ee;
+    padding: 1rem 2rem;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    outline: 0;
+    border: 0;
+    border-radius: 0.25rem;
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+    text-align: center;
+    display: inline-block
+  `
+
   return (
     <div
       onClick={handleOnClick}
-      css={css`
-        position: relative;
-        overflow: hidden;
-        transition: background 400ms;
-        color: #fff;
-        background-color: #6200ee;
-        padding: 1rem 2rem;
-        font-family: 'Roboto', sans-serif;
-        font-size: 1.5rem;
-        outline: 0;
-        border: 0;
-        border-radius: 0.25rem;
-        box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.3); /* black with 30% opacity */
-        cursor: pointer;
-        text-align: center;
-      `}
+      css={baseButton}
     >
       { children }
     </div>
