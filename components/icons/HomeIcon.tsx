58a637ba0e70a492ab52b33fdb3c94d31cb90ec7
trastanechora <trastanechora@gmail.com>
2021-04-10 10:32:15 +0700

    Add home icon

---
+++ components/icons/HomeIcon.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+function HomeIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" width="24" height="24" {...props}>
+      <path fill={props.color} d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" />
+    </svg>
+  )
+}
+
+export default HomeIcon
