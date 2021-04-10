efc69ee606a57d60b04af320eee78cddbe07552e
trastanechora <trastanechora@gmail.com>
2021-04-10 22:13:31 +0700

    Take fetch graphql from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,11 @@
 import DefaultLayout from '../layout/Default'
 import { useQuery, gql } from '@apollo/client';
-import ListCard from '../components/ListCard'
-
-const GET_POKEMONS = gql`
-  query pokemons($limit: Int, $offset: Int) {
-    pokemons(limit: $limit, offset: $offset) {
-      count
-      next
-      previous
-      status
-      message
-      results {
-        url
-        name
-        image
-      }
-    }
-  }
-`;
-
-const gqlVariables = {
-  limit: 2,
-  offset: 1,
-};
-
-export const FetchPokemon = () => {
-  const { loading, error, data } = useQuery(GET_POKEMONS, {
-    variables: gqlVariables,
-  });
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error! {error.message}</div>;
-
-  console.warn('Response from server', data);
-  return <div>Success!</div>;
-};
+import PokemonList from '../components/PokemonList'
 
 const Home = () => {
   return (
     <DefaultLayout headerTitle="Pokemon App">
-      <ListCard />
+      <PokemonList />
     </DefaultLayout>
   )
 }
