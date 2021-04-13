4e30980a7bf88e038d6d780434f48f67245bb880
trastanechora <trastanechora@gmail.com>
2021-04-14 01:52:52 +0700

    Add release icon

---
+++ components/icons/ReleaseIcon.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+function ReleaseIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" width="24" height="24" {...props}>
+      <path
+        fill={props.color}
+        d="M19,1L17.74,3.75L15,5L17.74,6.26L19,9L20.25,6.26L23,5L20.25,3.75M9,4L6.5,9.5L1,12L6.5,14.5L9,20L11.5,14.5L17,12L11.5,9.5M19,15L17.74,17.74L15,19L17.74,20.25L19,23L20.25,20.25L23,19L20.25,17.74"
+      />
+    </svg>
+  );
+}
+
+export default ReleaseIcon;
