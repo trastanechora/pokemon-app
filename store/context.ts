31f3689d5bd8f87dc4a0f23ad96e65b691ac3c6f
trastanechora <trastanechora@gmail.com>
2021-04-13 23:07:23 +0700

    Update context usage

--- store/context.ts
+++ store/context.ts
@@ -1,26 +1,9 @@
 import { createContext } from 'react';
+import { PokemonState, PokemonActions } from '../types';
 
-export type PokemonActions =
-  | { type: 'ADD_MY_POKEMON'; payload: any }
-  | { type: 'REMOVE_MY_POKEMON'; payload: any };
-
-const STORAGE_KEY = 'pokemonState';
-
-interface PokemonState {
-  myPokemon: any;
-}
-const defaultState: PokemonState = {
-  myPokemon: []
+export const defaultState: any = {
+  myPokemons: []
 };
 
-export interface PokemonProviderValue {
-  state: PokemonState;
-  dispatch(action: PokemonActions): void;
-}
-
-const StoreContext = createContext({
-  state: defaultState,
-  dispatch: (action: PokemonActions) => {}
-});
-
-export default StoreContext;
+export const store = createContext(defaultState);
+export const { Provider } = store;
