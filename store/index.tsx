3126b754baadfb10882732dfb6cf71abc2b82b1f
trastanechora <trastanechora@gmail.com>
2021-04-13 23:07:05 +0700

    Add context powered store

---
+++ store/index.tsx
@@ -0,0 +1,15 @@
+import { useReducer } from 'react';
+import { defaultState, Provider, store } from './context';
+import CustomReducer from './reducer';
+import { PokemonState, PokemonActions } from '../types';
+
+type Reducer<S, A> = (prevState: S, action: A) => S;
+
+const StateProvider = ( { children } ) => {
+  const [state, dispatch] = useReducer<Reducer<PokemonState, PokemonActions>>(CustomReducer, defaultState)
+  const providerValue = { state, dispatch };
+
+  return <Provider value={providerValue}>{children}</Provider>;
+};
+
+export { store, StateProvider }
