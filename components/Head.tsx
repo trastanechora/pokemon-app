ab6d13d8b797e764d1984057c478e7ad78b26653
trastanechora <trastanechora@gmail.com>
2021-04-14 10:32:15 +0700

    Modify head props

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 
 export interface IHeadComponentProps {
-  title?: string;
-  description?: string;
+  title: string;
+  description: string;
 }
 
 const HeadComponent = ({ title, description }: IHeadComponentProps) => {
