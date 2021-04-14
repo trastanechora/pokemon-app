c2370db77b68f1b2f70cb943391216161e7e9df2
trastanechora <trastanechora@gmail.com>
2021-04-14 09:34:16 +0700

    Add head component for dynamic meta SEO

---
+++ components/Head.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export interface IHeadComponentProps {
+  title?: string;
+  description?: string;
+}
+
+const HeadComponent = ({ title, description }: IHeadComponentProps) => {
+  return (
+    <Head>
+      <meta name="description" content={description} />
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+export default HeadComponent;
